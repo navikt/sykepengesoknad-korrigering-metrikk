47daa5cb2e7f7b3527dee355ad6d4c562192a826
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-11-09 12:04:28 +0100

    Metrikk på sendt før tom

--- src/main/kotlin/no/nav/helse/flex/kafka/SykepengesoknadListener.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/SykepengesoknadListener.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.metrikker.JobbetUnderveisTimerProsent
 import no.nav.helse.flex.metrikker.KorrigerteSoknader
+import no.nav.helse.flex.metrikker.SoknadSendtForTom
 import no.nav.helse.flex.metrikker.StudierEtterBegyntSykefravaer
 import no.nav.helse.flex.objectMapper
 import no.nav.syfo.kafka.felles.SykepengesoknadDTO
@@ -17,25 +18,23 @@ class SykepengesoknadListener(
     private val korrigerteSoknader: KorrigerteSoknader,
     private val jobbetUnderveisTimerProsent: JobbetUnderveisTimerProsent,
     private val studierEtterBegyntSykefravaer: StudierEtterBegyntSykefravaer,
+    private val soknadSendtForTom: SoknadSendtForTom,
 ) {
 
     private val log = logger()
 
     @KafkaListener(topics = [FLEX_SYKEPENGESOKNAD_TOPIC])
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
-        try {
-            val soknad = cr.value().tilSykepengesoknadDTO()
 
-            log.debug("Mottok soknad ${soknad.id} med status ${soknad.status}")
+        val soknad = cr.value().tilSykepengesoknadDTO()
 
-            korrigerteSoknader.finnKorrigerteSporsmal(soknad)
-            jobbetUnderveisTimerProsent.finnForetrukketSvarJobbetUnderveis(soknad)
-            studierEtterBegyntSykefravaer.finnBegyntStudierFoerSyk(soknad)
-            acknowledgment.acknowledge()
-        } catch (e: Exception) {
-            log.error("Feil ved mottak av record med key: ${cr.key()} offset: ${cr.offset()} partition: ${cr.partition()}", e)
-            throw e
-        }
+        log.debug("Mottok soknad ${soknad.id} med status ${soknad.status}")
+
+        korrigerteSoknader.finnKorrigerteSporsmal(soknad)
+        jobbetUnderveisTimerProsent.finnForetrukketSvarJobbetUnderveis(soknad)
+        studierEtterBegyntSykefravaer.finnBegyntStudierFoerSyk(soknad)
+        soknadSendtForTom.finnSoknadSendtForTom(soknad)
+        acknowledgment.acknowledge()
     }
 
     fun String.tilSykepengesoknadDTO(): SykepengesoknadDTO = objectMapper.readValue(this)
