1d25fbca6e04882b53c77ce12795aa52f4e9e23d
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-06-09 13:46:47 +0200

    Litt logging

--- src/main/kotlin/no/nav/helse/flex/bigquery/TableCreator.kt
+++ src/main/kotlin/no/nav/helse/flex/bigquery/TableCreator.kt
@@ -35,14 +35,21 @@ class TableCreator(
     }
 
     fun createTable(tableName: String, schema: Schema) {
+
+        val table = bigQuery.getTable(TableId.of(dataset, tableName))
+        if (table != null && table.exists()) {
+            log.info("Table $tableName eksisterer allerede")
+            return
+        }
+
         try {
             // Initialize client that will be used to send requests. This client only needs to be created
             // once, and can be reused for multiple requests.
             val tableId = TableId.of(dataset, tableName)
             val tableDefinition: TableDefinition = StandardTableDefinition.of(schema)
             val tableInfo = TableInfo.newBuilder(tableId, tableDefinition).build()
-            bigQuery.create(tableInfo)
-            log.info("Table $tableName created successfully")
+            val create = bigQuery.create(tableInfo)
+            log.info("Table $tableName created successfully: ${create.tableId.iamResourceName}")
         } catch (e: BigQueryException) {
             log.error("Table was not created.,", e)
             throw e
