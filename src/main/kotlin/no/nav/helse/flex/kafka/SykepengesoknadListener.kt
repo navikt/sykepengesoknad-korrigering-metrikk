232db874f8bd74707364851282b9038aef4f54ff
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-06-09 12:47:54 +0200

    Insert i bq test

--- src/main/kotlin/no/nav/helse/flex/kafka/SykepengesoknadListener.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/SykepengesoknadListener.kt
@@ -1,17 +1,23 @@
 package no.nav.helse.flex.kafka
 
 import com.fasterxml.jackson.module.kotlin.readValue
+import com.google.cloud.bigquery.BigQueryOptions
+import com.google.cloud.bigquery.InsertAllRequest
+import com.google.cloud.bigquery.TableId
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.metrikker.JobbetUnderveisTimerProsent
 import no.nav.helse.flex.metrikker.KorrigerteSoknader
 import no.nav.helse.flex.metrikker.SoknadSendtForTom
 import no.nav.helse.flex.metrikker.StudierEtterBegyntSykefravaer
 import no.nav.helse.flex.objectMapper
-import no.nav.helse.flex.sykepengesoknad.kafka.*
+import no.nav.helse.flex.sykepengesoknad.kafka.SykepengesoknadDTO
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
+import java.time.Instant
+import java.util.*
+import kotlin.collections.HashMap
 
 @Component
 class SykepengesoknadListener(
@@ -22,12 +28,23 @@ class SykepengesoknadListener(
 ) {
 
     private val log = logger()
+    val bigquery = BigQueryOptions.getDefaultInstance().service
 
     @KafkaListener(topics = [FLEX_SYKEPENGESOKNAD_TOPIC])
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
 
         val soknad = cr.value().tilSykepengesoknadDTO()
 
+        val row1Data: MutableMap<String, Any> = HashMap()
+        row1Data["booleanField"] = true
+        row1Data["soknadid"] = soknad.id
+        row1Data["status"] = soknad.status
+        row1Data["opprettet"] = Instant.now()
+        bigquery.insertAll(
+            InsertAllRequest.newBuilder(TableId.of("korrigering_metrikk", "soknadtest"))
+                .addRow(UUID.randomUUID().toString(), row1Data)
+                .build()
+        )
         log.debug("Mottok soknad ${soknad.id} med status ${soknad.status}")
 
         korrigerteSoknader.finnKorrigerteSporsmal(soknad)
